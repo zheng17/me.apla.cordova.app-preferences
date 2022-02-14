c5375d406c0bed7becbd7fa782575cee7ff8f5bb
Zheng <luzhengno1@gmail.com>
2022-02-14 01:13:03 -0600

    cordova 10

--- bin/lib/android.js
+++ bin/lib/android.js
@@ -4,7 +4,7 @@ var mappings = require("./mappings"),
 module.exports = function (context) {
 
 	var
-		req = context ? context.requireCordovaModule : require,
+		req = require,
 		Q = req('q'),
 		path = req('path'),
 		ET = req('elementtree'),
